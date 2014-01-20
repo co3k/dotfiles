9b057d5a946a33b3e43ef0b47a731232ad228411
Kousuke Ebihara <kousuke@co3k.org>
2014-01-20 20:50:35 +0900

    added some pictures

--- install.sh
+++ install.sh
@@ -23,6 +23,11 @@ ln -s $BASE_PATH/.muttrc ~/.muttrc
 ln -s $BASE_PATH/_gitconfig ~/.gitconfig
 ln -s $BASE_PATH/_gitignore ~/.gitignore
 
+mkdir -p ~/Pictures
+rm ~/Pictures/kioku
+ln -s $BASE_PATH/pictures/kioku ~/Pictures/kioku
+ln -s $BASE_PATH/pictures/ebihara-150x150.jpg ~/Pictures/ebihara-150x150.jpg
+
 mkdir -p ~/.mutt
 curl https://raw.github.com/altercation/mutt-colors-solarized/master/mutt-colors-solarized-dark-16.muttrc > ~/.mutt/mutt-colors-solarized-dark-16.muttrc
 
